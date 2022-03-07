6d1e8b20177e6646c0a13f2431cbf91d4b4d0477
duahau13 <56484469+duahau13@users.noreply.github.com>
2022-03-07 15:33:48 +0700

    style

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 Vue.createApp({
   data() {
     return {
-			url:'http://abc.com',
-			showBook: true,
-			books: [
-				{title: 'The Final Empire', author: 'Brandon Williams', img: 'assets/1.jpg', isFav: true},
-				{title: 'Name of the Wind', author: 'Patrick Swayne', img: 'assets/2.jpg',  isFav: false},
-				{title: 'A song of fire and ice', author: 'George RR Martin', img: 'assets/3.jpg',  isFav: true}
-			]
-      
-    }
+      url: "http://abc.com",
+      showBook: true,
+      books: [
+        {
+          title: "The Final Empire",
+          author: "Brandon Williams",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "Name of the Wind",
+          author: "Patrick Swayne",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "A song of fire and ice",
+          author: "George RR Martin",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
+    };
   },
-	methods: {
-		changeTitle(title) {
-			this.title = title
-		},
-		toggleShowBooks (){
-			this.showBook = !this.showBook
-		},
-		toggleFav (book){
-			book.isFav = !book.isFav
-		}
-	}
-}).mount('#app')
+  methods: {
+    changeTitle(title) {
+      this.title = title;
+    },
+    toggleShowBooks() {
+      this.showBook = !this.showBook;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filterBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+}).mount("#app");
